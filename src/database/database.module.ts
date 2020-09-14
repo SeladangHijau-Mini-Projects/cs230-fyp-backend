7521692ef5536840b2d09ef3a3e93c5133fd1d1f
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-14 20:41:29 +0800

    updated env example

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-
 const mongooseModule = MongooseModule.forRootAsync({
   useFactory: async () => {
-      const { MONGODB_ENDPOINT, NODE_ENV } = process.env;
+      const { MONGODB_ENDPOINT } = process.env;
 
       if (!MONGODB_ENDPOINT) throw new Error('Missing env: MONGODB_ENDPOINT');
 
@@ -14,22 +13,6 @@ const mongooseModule = MongooseModule.forRootAsync({
           useCreateIndex: true,
           useNewUrlParser: true,
       };
-
-      if (NODE_ENV && NODE_ENV.toLowerCase() === 'production') {
-          const { USE_TLS, CA_FILE, CERT_FILE } = process.env;
-
-          if (!USE_TLS) throw new Error('Missing env: USE_TLS');
-
-          if (!CA_FILE) throw new Error('Missing env: CA_FILE');
-
-          if (!CERT_FILE) throw new Error('Missing env: CERT_FILE');
-
-          Object.assign(connConfig, {
-              tls: process.env.USE_TLS,
-              tlsCAFile: process.env.CA_FILE,
-              tlsCertificateKeyFile: process.env.CERT_FILE,
-          });
-      }
 
       return connConfig;
   },
