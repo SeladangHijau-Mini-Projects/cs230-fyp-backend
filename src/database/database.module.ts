b6802371e70f15d145603c9261f68c400f4191f4
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-14 17:12:37 +0800

    setup MongoDB connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,41 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.provider';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+const mongooseModule = MongooseModule.forRootAsync({
+  useFactory: async () => {
+      const { MONGODB_ENDPOINT, NODE_ENV } = process.env;
+
+      if (!MONGODB_ENDPOINT) throw new Error('Missing env: MONGODB_ENDPOINT');
+
+      const connConfig = {
+          uri: MONGODB_ENDPOINT,
+          useFindAndModify: false,
+          useCreateIndex: true,
+          useNewUrlParser: true,
+      };
+
+      if (NODE_ENV && NODE_ENV.toLowerCase() === 'production') {
+          const { USE_TLS, CA_FILE, CERT_FILE } = process.env;
+
+          if (!USE_TLS) throw new Error('Missing env: USE_TLS');
+
+          if (!CA_FILE) throw new Error('Missing env: CA_FILE');
+
+          if (!CERT_FILE) throw new Error('Missing env: CERT_FILE');
+
+          Object.assign(connConfig, {
+              tls: process.env.USE_TLS,
+              tlsCAFile: process.env.CA_FILE,
+              tlsCertificateKeyFile: process.env.CERT_FILE,
+          });
+      }
+
+      return connConfig;
+  },
+});
 
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  imports: [mongooseModule]
 })
 export class DatabaseModule {}
--- src/setting/setting.module.ts
+++ src/setting/setting.module.ts
@@ -4,8 +4,15 @@ import { Setting, SettingSchema } from './schema/setting.schema';
 import { SettingController } from './setting.controller';
 import { SettingService } from './setting.service';
 
+const schemas = MongooseModule.forFeature([
+  {
+      name: Setting.name,
+      schema: SettingSchema,
+  },
+]);
+
 @Module({
-  imports: [MongooseModule.forFeature([{name: Setting.name, schema: SettingSchema}])],
+  imports: [schemas],
   controllers: [SettingController],
   providers: [SettingService],
 })
