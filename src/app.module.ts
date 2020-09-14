adeb1077e5b31bbb144142685d073c6fa9825a91
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-14 17:17:24 +0800

    removed app controller, service & spec file (minimize)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { SettingModule } from './setting/setting.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, SettingModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
