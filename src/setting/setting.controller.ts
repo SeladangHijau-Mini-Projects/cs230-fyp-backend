6c0c27f0c01424a4f31151189005b8ed9d359c98
nadzmi.idzham <nadzmi.idzham@carsome.com>
2020-09-14 22:06:36 +0800

    - added dockerignore - updated plural naming for setting

--- src/setting/setting.controller.ts
+++ src/setting/setting.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { Setting } from './schema/setting.schema';
 import { SettingService } from './setting.service';
 
-@Controller('setting')
+@Controller('settings')
 export class SettingController {
   constructor(private readonly settingService: SettingService) {}
 
